2c236af1d43c0d5a97486be3d3369d637b835110
Mobile User <mobile@ip6-dynamic-adsl.viettel.vn>
2025-08-09 09:59:25 +0700

    Changed 2 files Modified: public/go.html,server.js

--- server.js
+++ server.js
@@ -151,6 +151,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/go', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'go.html'));
+});
+
 // Constants
 const CHUNK_SIZE = 5 * 1024 * 1024;
 const MAX_CONCURRENT_DOWNLOADS = 2;
